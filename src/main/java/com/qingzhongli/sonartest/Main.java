f08c6d6742bc59529d33ff8b9e913efdf205cdff
qingzhongli <qingzhongli2018@gmail.com>
2024-02-26 10:53:51 +0800

    fix security hotspots reported by sonarcloud

--- src/main/java/com/qingzhongli/sonartest/Main.java
+++ src/main/java/com/qingzhongli/sonartest/Main.java
@@ -4,19 +4,22 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    private static final String ENDPOINT_REGEX = "[^\\n]?\\[?([0-9a-zA-Z\\-%._:]*)\\]?:(\\d+)";
+    // This matches URIs of formats: host:port and protocol://host:port
+    // IPv6 is supported with [ip] pattern
+    // Reference https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/utils/Utils.java#L97
+    private static final String ENDPOINT_REGEX = "[^\\n].*?\\[?([0-9a-zA-Z\\-%._:]*)\\]?:(\\d+)";
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            return;
-        }
-
-        String test = args[0];
-        if (!Pattern.matches(ENDPOINT_REGEX, test)) {
-            System.out.println(test + "not match pattern " + ENDPOINT_REGEX);
-        } else {
-            System.out.println("Hello World!");
-        }
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, ""));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "test"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "[2001:db8:85a3::8a2e:370:7334]:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "[2001:db8:85a3::8a2e:370:7334]:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "[2001:0db8:85a3:0000:0000:8a2e:0370:7335]:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "192.168.164.136:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "PLAINTEXT://[2001:db8:85a3::8a2e:370:7334]:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "SSL://[2001:db8:85a3::8a2e:370:7334]:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "SASL_PLAINTEXT://[2001:0db8:85a3:0000:0000:8a2e:0370:7335]:9092"));
+        System.out.println(Pattern.matches(ENDPOINT_REGEX, "SASL_SSL://192.168.164.136:9092"));
     }
 
 }
