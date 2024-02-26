8013d3c7393eaf88130fd32eeaf67999c53baad7
qingzhongli <qingzhongli2018@gmail.com>
2024-02-26 10:03:21 +0800

    first commit

---
+++ src/main/java/com/qingzhongli/sonartest/Main.java
@@ -0,0 +1,22 @@
+package com.qingzhongli.sonartest;
+
+import java.util.regex.Pattern;
+
+public class Main {
+
+    private static final String ENDPOINT_REGEX = ".*?\\[?([0-9a-zA-Z\\-%._:]*)\\]?:(\\d+)";
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            return;
+        }
+
+        String test = args[0];
+        if (!Pattern.matches(ENDPOINT_REGEX, test)) {
+            System.out.println(test + "not match pattern " + ENDPOINT_REGEX);
+        } else {
+            System.out.println("Hello World!");
+        }
+    }
+
+}
